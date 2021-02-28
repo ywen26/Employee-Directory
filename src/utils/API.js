98c0f9f218d90becc3e2568f00d0c40a66553dca
Wednesday <yensonyu@gmail.com>
2021-02-27 20:57:42 -0800

    create API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,1 +1,9 @@
-// import axios from "axios";
+import axios from "axios";
+
+const API = {
+    getEmployeesList: function() {
+        return axios.get("https://randomuser.me/api/?results=50");
+    }
+}
+
+export default API;
