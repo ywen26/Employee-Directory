bf527150c420a55c20d02f24ec032f66250508a7
Yuwen Yu <yensonyu@gmail.com>
2021-03-03 23:55:43 -0800

    Delete src directory

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = {
-    getEmployeesList: function() {
-        return axios.get("https://randomuser.me/api/?results=50&nat=us");
-    }
-}
-
-export default API;
